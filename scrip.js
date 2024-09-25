17989edca5fcc06e7c890a49996efebd2462b7c1
HermesHerasme <hermesalberto@Hermess-MacBook-Air.local>
2024-09-24 23:13:06 -0400

    trabajando en el js

--- scrip.js
+++ scrip.js
@@ -1,10 +1,33 @@
 const addTask = document.querySelector('.text')
-let FinalTask = document.querySelector('.task1')
+const fialtask = document.querySelector('.task1')
 const submit = document.querySelector('.submit')
+const taskContainer = document.querySelector('.taskContainer')
+const taskManager =`
+<div class="taskManager">
 
+    <input class="radiobtn" type="radio">  
+    <div class="task1"></div>           
+    <button class="btn">X</button>
+                
+</div>
+`;
 
 
 submit.addEventListener('click', () =>{
+
+    if(addTask.value.trim() === " "){
+        alert('pleace add a task')
+
+    }else{
+    taskContainer.insertAdjacentHTML('beforeend',taskManager)
+    const newFinaltask = document.querySelector('.taskManager:last-child .task1')
+
+    
+       fialtask.textContent = addTask.textContent
+    
+
+
+    }
     
 
 
